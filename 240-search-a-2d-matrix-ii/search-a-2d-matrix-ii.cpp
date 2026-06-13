df1455b8f79f4ed16c455b1952b84269eae5eb43
Omkar-Hajare <hajareom6550@gmail.com>
2026-06-13 18:56:51 +0530

    Time: 51 ms (69.66%) | Memory: 18.8 MB (37.06%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
-     bool check(vector<int>& arr, int target,int n) {
-            int start = 0;
-            int end = n- 1;
+    //  bool check(vector<int>& arr, int target,int n) {
+    //         int start = 0;
+    //         int end = n- 1;
 
-            while (start <= end) {
-                int mid = (start + end) / 2;
-                if (arr[mid] == target) {
-                    return true;
-                }else if(arr[mid]<target){
-                    start=mid+1;
-                }else{
-                    end=mid-1;
-                }
-            }
-            return false;
-        }
+    //         while (start <= end) {
+    //             int mid = (start + end) / 2;
+    //             if (arr[mid] == target) {
+    //                 return true;
+    //             }else if(arr[mid]<target){
+    //                 start=mid+1;
+    //             }else{
+    //                 end=mid-1;
+    //             }
+    //         }
+    //         return false;
+    //     }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix.size();
-        int n=matrix[0].size();
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int row = 0;
+        int col = n - 1;
 
-        for(int i=0;i<m;i++){
-           if( check(matrix[i], target ,n)){
-            return true;
-           }
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
         }
         return false;
+
+        // for(int i=0;i<m;i++){
+        //    if( check(matrix[i], target ,n)){
+        //     return true;
+        //    }
+        // }
+        // return false;
     }
 };
