03b2aad43761851e74a05a96dc89e2f8a976c10d
Omkar-Hajare <hajareom6550@gmail.com>
2026-06-17 13:46:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (75.72%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        int m1[26] = {0}, m2[26] = {0}; 
+        int m1[26] = {0};
+
+        //adding frequency 
         for(int i=0;i<s.size();i++){
             m1[s[i]-'a']++;
-            m2[t[i]-'a']++;
         }
 
+        //decreasing frequence
+        for(int i=0;i<s.size();i++){
+            m1[t[i]-'a']--;
+        }
+
+        //if all elements are match the frequency return to 0
         for (int i = 0; i < 26; ++i) {
-              if (m1[i] != m2[i]) return false;
+              if (m1[i] !=0) return false;
         }
         return true;
     }
