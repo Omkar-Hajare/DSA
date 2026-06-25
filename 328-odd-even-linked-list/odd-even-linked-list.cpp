d223df953b5d802689edb4ad1f45703aea4b5e7a
Omkar-Hajare <hajareom6550@gmail.com>
2026-06-25 20:52:43 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (88.79%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,39 +11,25 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head == NULL || head->next == NULL)
-    return head;
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
         ListNode* odd=head;
-        ListNode* slow=head->next;
-        vector<int> arr;
+        ListNode* even=head->next;
+        ListNode* evenhead=head->next;
 
-      while(odd!=NULL){
-        arr.push_back(odd->val);
-        if(odd->next){
-        odd=odd->next->next;
-        }else{
-            break;
+        while(even!=NULL && even->next!=NULL ){
+            odd->next=odd->next->next;
+            odd=odd->next;
+            even->next=even->next->next;
+            even=even->next;
         }
-      }
 
-      ListNode* even=head->next;
-      while(even!=NULL){
-        arr.push_back(even->val);
-        if(even->next){
-        even=even->next->next;
-        }else{
-            break;
-        }
-      }
+        odd->next=evenhead;
 
-      ListNode* temp=head;
-      int i=0;
-      while(temp!=NULL){
-        temp->val=arr[i];
-        temp=temp->next;
-        i++;
-      }
 
-      return head;
+        return head;
+
+
     }
 };
