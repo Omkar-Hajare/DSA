02469a1a074543f1a0f575d29a64a9ee9b4fb2b0
Omkar-Hajare <hajareom6550@gmail.com>
2026-07-04 20:18:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (25.94%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+
+        int ans=start^goal;
+        int count=0;
+
+        for(int i=0;i<31;i++){
+            count += (ans & 1); 
+            ans = ans >> 1;
+        }
+        
+        return count;
+    }
+};
