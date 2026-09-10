dc471a09855e383136f09d5d7910f5d37dd0ba1c
Omkar-Hajare <hajareom6550@gmail.com>
2026-09-10 23:35:54 +0530

    Time: 0 ms (100.00%) | Memory: 87.3 MB (44.68%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-      int five = 0; 
-        int ten = 0; 
+        int five = 0;
+        int ten = 0;
 
-        for (int bill : bills) {
-            if (bill == 5) {
+        for (int num : bills) {
+            if (num == 5) {
                 five++;
-            }
-            else if (bill == 10) {
+            } else if (num == 10) {
                 if (five > 0) {
-                    five--; 
-                    ten++; 
+                    five--;
+                    ten++;
                 } else {
-                    return false; 
+                    return false;
                 }
-            }
-            else { 
-                if (five > 0 && ten > 0) {
-                    five--; 
-                    ten--;  
-                } 
-                else if (five >= 3) {
-                    five -= 3; 
-                } 
-                else {
+            } else {
+                if (five > 0 & ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
                     return false;
                 }
             }
         }
-        return true; 
+        return true;
     }
+    
 };
